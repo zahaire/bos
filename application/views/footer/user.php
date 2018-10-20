1bb2c70daa735d79e406a0f5ff5bf5c214b8bc20
jezergutierrez <jezergutierrez@gmail.com>
2018-10-20 16:23:06 +0800

    initial commit

---
+++ application/views/footer/user.php
@@ -0,0 +1,33 @@
+<!--Footer-->
+<footer class="page-footer text-center font-small mt-4 wow fadeIn">
+
+
+<!--Copyright-->
+<div class="footer-copyright py-3">
+  © 2018 Copyright:
+  <a href="#" target="_blank"> Better Online Survey </a>
+</div>
+<!--/.Copyright-->
+
+</footer>
+<!--/.Footer-->
+
+  
+
+<!-- SCRIPTS -->
+<!-- JQuery -->
+<script src="<?=base_url('assets/mdb/js/jquery-3.3.1.min.js')?>"></script>
+<!-- Bootstrap tooltips -->
+<script src="<?=base_url('assets/mdb/js/popper.min.js')?>"></script>
+<!-- Bootstrap core JavaScript -->
+<script src="<?=base_url('assets/mdb/js/bootstrap.min.js')?>"></script>
+<!-- MDB core JavaScript -->
+<script src="<?=base_url('assets/mdb/js/mdb.min.js')?>"></script>
+<!-- Initializations -->
+<script type="text/javascript">
+// Animations initialization
+new WOW().init();
+</script>
+</body>
+
+</html>
---
+++ application/views/nav/user.php
@@ -0,0 +1,126 @@
+<!-- Navbar -->
+<nav class="navbar fixed-top navbar-expand-lg navbar-light white scrolling-navbar">
+	<div class="container">
+
+		<!-- Brand -->
+		<a class="navbar-brand waves-effect" href="#" target="_blank">
+			<strong class="blue-text">Better Online Survey</strong>
+		</a>
+
+		<!-- Collapse -->
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+		 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+
+		<!-- Links -->
+		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+			<!-- Left -->
+			<ul class="navbar-nav mr-auto">
+				<li class="nav-item">
+					<a class="nav-link waves-effect" href="<?=base_url();?>Home/index">Home
+					</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link waves-effect" href="#" target="_blank">About</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link waves-effect" href="">Products</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link waves-effect" href="#" target="_blank">Services</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link waves-effect" href="#" target="_blank">Contact</a>
+				</li>
+			</ul>
+
+			<!-- Right -->
+			<ul class="navbar-nav nav-flex-icons">
+
+
+				<li class="nav-item">
+					<a class="nav-link" data-toggle="modal" data-target="#exampleModal">
+						<i class="fa fa-sign-in mr-2"></i>Login
+					</a>
+				</li>
+
+				<li class="nav-item">
+					<a class="nav-link" data-toggle="modal" data-target="#exampleModal1">
+						<i class="fa fa-user mr-2"></i>Register
+					</a>
+
+				</li>
+			</ul>
+
+		</div>
+
+	</div>
+</nav>
+<!-- Navbar -->
+
+<div class="modal fade" id="exampleModal1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+	<div class="modal-dialog" role="document">
+		<div class="modal-content">
+			<div class="modal-body">
+				<div class="modal-header text-center">
+					<h4 class="modal-title w-100 font-weight-bold">Register</h4>
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+				</div>
+				<div class="modal-body mx-3">
+					<div class="md-form mb-5">
+						<i class="fa fa-envelope prefix grey-text"></i>
+						<input type="email" id="defaultForm-email" class="form-control validate">
+						<label data-error="wrong" data-success="right" for="defaultForm-email">Your email</label>
+					</div>
+
+					<div class="md-form mb-4">
+						<i class="fa fa-lock prefix grey-text"></i>
+						<input type="password" id="defaultForm-pass" class="form-control validate">
+						<label data-error="wrong" data-success="right" for="defaultForm-pass">Your password</label>
+					</div>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+					<button type="button" class="btn btn-primary">Login</button>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+<div class="modal fade" id="exampleModal" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+	<div class="modal-dialog" role="document">
+		<div class="modal-content">
+			<div class="modal-body">
+				<div class="modal-header text-center">
+					<h4 class="modal-title w-100 font-weight-bold">Sign in</h4>
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+				</div>
+				<div class="modal-body mx-3">
+					<div class="md-form mb-5">
+						<i class="fa fa-envelope prefix grey-text"></i>
+						<input type="email" id="defaultForm-email" class="form-control validate">
+						<label data-error="wrong" data-success="right" for="defaultForm-email">Your email</label>
+					</div>
+
+					<div class="md-form mb-4">
+						<i class="fa fa-lock prefix grey-text"></i>
+						<input type="password" id="defaultForm-pass" class="form-control validate">
+						<label data-error="wrong" data-success="right" for="defaultForm-pass">Your password</label>
+					</div>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+					<button type="button" class="btn btn-primary">Login</button>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
