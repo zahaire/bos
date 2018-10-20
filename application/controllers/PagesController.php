5737925bb9f6c3391914d6ef33ac0caa1304dfe0
ceddyjohn <cedricureta@gmail.com>
2018-10-20 16:51:56 +0800

    ureta

---
+++ application/controllers/PagesController.php
@@ -0,0 +1,31 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class PagesController extends CI_Controller {
+
+    public function __construct() {
+        parent::__construct();
+        $this->load->helper('url');
+        $this->load->library('session');
+        // loading captcha helper
+        // $this->load->helper('captcha');
+        $this->load->library('form_validation');
+    }
+
+    public function index() {
+        $this->load->view('nav/user.php');
+        $this->load->view('header/index.php');
+        $this->load->view('footer/user.php');
+
+    }
+    
+    public function product() {
+        $this->load->view('header/customer.php');
+        $this->load->view('nav/customer.php');
+        $this->load->view('body/customer/home.php');
+        $this->load->view('footer/customer.php');
+    
+    }
+
+}
